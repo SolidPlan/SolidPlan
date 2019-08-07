2953a118a2e830ba245a6cec39a661d35f95abf0
Pierre du Plessis <pierre@pcservice.co.za>
2019-08-07 14:09:38 +0200

    Initial ui structure

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/ui/$1',
+    '^~/(.*)$': '<rootDir>/ui/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  'collectCoverage': true,
+  'collectCoverageFrom': [
+    '<rootDir>/ui/components/**/*.vue',
+    '<rootDir>/ui/pages/**/*.vue'
+  ]
+}
