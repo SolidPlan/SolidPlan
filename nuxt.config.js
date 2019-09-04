f88e3361d2c5c8a7322fca7260d16034f3cdfb0c
Pierre du Plessis <pierre@pcservice.co.za>
2019-09-04 16:47:56 +0200

    Remove custom dark theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,15 +101,6 @@ export default {
     theme: {
       dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.lighten3,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        },
         light: {
           primary: '#4caf50',
           secondary: '#4caf50',
