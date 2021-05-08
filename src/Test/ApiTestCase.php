fe68f23216fadc77466efb8e3cc8dcd396daa043
Pierre du Plessis <pierre@pcservice.co.za>
2021-05-08 10:31:14 +0200

    Upgrade to Symfony 4 and PHPUnit 8

--- src/Test/ApiTestCase.php
+++ src/Test/ApiTestCase.php
@@ -35,7 +35,7 @@ abstract class ApiTestCase extends WebTestCase
 
     private static $jwt;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
